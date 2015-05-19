c209abe191ad393e5e5dca8cc40b164a8687e58b
Yue Chu <ychu43@gatech.edu>
2015-05-19 15:31:01 -0400

    JavaDoc for Player

--- src/pong/Player.java
+++ src/pong/Player.java
@@ -15,22 +15,42 @@ public class Player {
         this.paddle = paddle;
     }
 
+    /**
+     * Set the score for the player
+     * @param score the new score
+     */
     public void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     * Returns the current score of the player
+     * @return the current score
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Sets the Paddle of the player
+     * @param paddle the Paddle for the player
+     */
     public void setPaddle(Paddle paddle) {
         this.paddle = paddle;
     }
 
+    /**
+     * Returns the current Paddle of the Player
+     * @return the current Paddle
+     */
     public Paddle getPaddle() {
         return paddle;
     }
 
+    /**
+     * Returns a string representation of the player.
+     * @return a string representation of the player
+     */
     public String toString() {
         return name;
     }
