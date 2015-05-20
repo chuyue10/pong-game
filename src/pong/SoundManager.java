7fba989cbfc19edac7b562d2d46d21f2fbbbaaa0
Yue Chu <ychu43@gatech.edu>
2015-05-19 20:18:04 -0400

    Added Sound Manager

---
+++ src/pong/SoundManager.java
@@ -0,0 +1,52 @@
+package pong;
+
+import javafx.scene.media.AudioClip;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by Charles on 5/19/2015.
+ */
+public class SoundManager {
+
+    // Singleton instance of the class
+    private static SoundManager soundManager;
+
+    // Pool thread for playing the sounds
+    private final ExecutorService soundPool;
+
+    // Map for storing resource addresses
+    Map<String, AudioClip> map = new HashMap<>();
+
+    private SoundManager(int threadCount) {
+        this.soundPool = Executors.newFixedThreadPool(threadCount);
+    }
+
+    public static SoundManager getInstance(int threadCount) {
+        if (soundManager == null) {
+            return new SoundManager(threadCount);
+        } else {
+            return soundManager;
+        }
+    }
+
+    public void loadSound(String id, URL url) {
+        AudioClip sound = new AudioClip(url.toExternalForm());
+        map.put(id, sound);
+    }
+
+    public void playSound(final String id) {
+        Runnable playSoundRunnable = (
+                () -> map.get(id).play()
+                );
+        soundPool.execute(playSoundRunnable);
+    }
+
+    public void shutdown() {
+        soundPool.shutdown();
+    }
+}
