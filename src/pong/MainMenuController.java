772a9df837bacd843c52703503d67e09366cfb47
Yue Chu <ychu43@gatech.edu>
2015-05-22 05:05:40 -0400

    Created controller for main menu

---
+++ src/pong/MainMenuController.java
@@ -0,0 +1,17 @@
+package pong;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Charles on 5/22/2015.
+ */
+public class MainMenuController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
